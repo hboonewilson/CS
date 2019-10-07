cd1757f38fa4de167f8d5d06d23ed67961aaf167
Henry Boone Wilson <hbwilson@uiowa.edu>
2019-10-07 14:56:53 -0500

    3:00pm 10/7/19

--- ProgPractice2.py
+++ ProgPractice2.py
@@ -1,11 +1,18 @@
-#pass as an argument a letter and a list... iterate through list looking for all
-#indexes that have that letter (should not be case sensitive)
+#pass as an argument a letter and a list... iterate through string looking for all
+#indexes that have that letter (should not be case sensitive). Return 'letter not 
+#found in string' if no instances of the letter are found in myString. If there
+#are return those instances 
 
-def whereIsLet(let, myList):
+def whereIsLet(let, myString):
+    under_list = myString.lower()
     retlist = []
-    for i in range(0, len(myList)):
-        if let == myList[i]:
+    for i in range(0, len(myString)):
+        if let == under_list[i]:
             retlist.append(i)
         else:
             pass
-    return retlist
+    if len(retlist) == 0:
+        return 'Letter not found in string'
+    else:
+        return retlist 
+
